c05d0a2c30bf1d8bfb38b7046080eb8d231f625f
Boncom99 <josepbmoya@gmail.com>
2024-03-26 00:17:41 +0100

    create the link element instead of change them all

--- background.js
+++ background.js
@@ -31,19 +31,24 @@ function setTabProperties(newTitle, newIcon, newEmoji) {
   if(newTitle){
   document.title = newTitle;
   }
-  let newIconToInsert=newEmoji
   if(!newEmoji){
-    if(!newIcon){
-      return
+   //remove link by id
+    const link= document.getElementById('dynamic-favicon-by-tabify-extension-123')
+    if(link){
+      link.remove()
     }
-    newIconToInsert=newIcon
+    return
   }
   const head = document.getElementsByTagName('head')[0]; // Get the head element
-  const links = head.getElementsByTagName('link');
-  for (let i = 0; i < links.length; i++) {
-    if(links[i].getAttribute('rel').includes('icon')){
-          links[i].setAttribute('href', newIconToInsert);
-    }
+  //create  a new link with the icon
+  let link= document.getElementById('dynamic-favicon-by-tabify-extension-123')
+  if(!link){
+    link = document.createElement('link');
+    link.id ='dynamic-favicon-by-tabify-extension-123';
+    link.rel = 'icon';
+    link.type = 'image/png';
   }
+    link.href=newEmoji;
+   head.appendChild(link);
 }
 
