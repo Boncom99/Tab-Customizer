1924742ede577c742de74f9b08d7e7a9caf69b06
Boncom99 <josepbmoya@gmail.com>
2024-03-30 02:51:32 +0100

    gitignore and amplitude.ts

--- src/amplitude.ts
+++ src/amplitude.ts
@@ -2,7 +2,7 @@ import * as amplitude from '@amplitude/analytics-browser';
 import {config} from "../config";
 const isProd = process.env.NODE_ENV === 'production';
 const key = isProd? config.amplitudeApiKey: 'dev key';
-amplitude.init(key as string,{ defaultTracking: true, });
+amplitude.init(key ,{ defaultTracking: true, });
 
 
 export default amplitude;
